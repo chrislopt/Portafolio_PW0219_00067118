c9fe5c9f8b944b14e5f8ca0df033d816284e7247
Christian Jesus Lopez Tejada <54133052+chrislopt@users.noreply.github.com>
2019-09-27 23:01:45 -0600

    labo 5 terminado

---
+++ laboratorio/lab5/ejercicio 10.js
@@ -0,0 +1,11 @@
+var s; 
+var v1 = Math.abs(6); 
+var v2 = Math.abs(-6); 
+if (v1 == v2) { 
+    document.write("Absolute values are the same."); 
+} 
+else { 
+document.write("Absolute values are different."); 
+} 
+ 
+// Output: Absolute values are the same. 
